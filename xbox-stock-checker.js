38d17026480bf9102099052593fd7a686f112ae3
Zosoled <chris@theduncans.us>
2020-11-28 03:56:44 -0800

    Big update

--- xbox-stock-checker.js
+++ xbox-stock-checker.js
@@ -1,36 +1,52 @@
-const storePage = new URL('https://www.walmart.com/ip/Xbox-Series-X/443574645')
-const targetText = 'Add to cart'
-const yeah = new Audio('https://instantrimshot.com/audio/csi.mp3')
-
-let request = function(url, callback) {
-    let xhr = new XMLHttpRequest()
-    xhr.open('GET', url, true)
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            callback(xhr.response)
-        }
-    }
-    xhr.send()
-}
-let check = function() {
-    try {
-        request(storePage, response => {
-            if (response.search('>'+targetText+'<') > -1) {
-                console.warn(new Date().toLocaleTimeString() + ' | IN STOCK')
-                yeah.play()
+    let xbox = (function() {
+        const yeah = new Audio('https://instantrimshot.com/audio/csi.mp3')
+        const buttonRegexList = [/add to cart/i, /ship it/i]
+        
+        function get(url, callback) {
+            let xhr = new XMLHttpRequest()
+            xhr.open('GET', url, true)
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    callback(xhr.response)
+                }
             }
-            setTimeout(check, 1000)
-        })
-    } catch (err) {
-        console.error('Xbox Stock Checker failed.\n' + err)
-    }
-}
-let start = function() {
-    if (window.location.hostname != storePage.hostname) {
-        console.error('You must run this from ' + storePage.hostname)
-    } else {
-        setTimeout(check, 1000)
-        return '\nXbox Stock Checker is running. Do not close this tab.\n'
-    }
-}
-start()
+            xhr.send()
+        }
+        
+        function check() {
+            try {
+                get(window.location.href, response => {
+                    buttonRegexList.forEach(regex => {
+                        if (regex.test(response)) {
+                            console.warn(new Date().toLocaleTimeString() + ' | IN STOCK')
+                            yeah.play()
+                        }
+                    })
+                })
+                setTimeout(check, 1000)
+            } catch (err) {
+                console.timeEnd('xbox-stock-checker')
+                console.error('Xbox Stock Checker failed.\n' + err)
+            }
+        }
+        
+        let start = new Date()
+        setTimeout(check)
+        console.info(`%cXbox Stock Checker%c is running in the background.
+Do not close this tab.
+Call %cxbox.runtime()%c to view elapsed time.`,
+            'background-color:#107c10;','','background-color:#33c;',''
+        )
+
+        return {
+            runtime: function() {
+                let now = new Date()
+                let diff = now - start
+                let days = Math.floor(diff / 86400000)
+                let hours = Math.floor((diff -= days * 86400000) / 3600000)
+                let minutes = Math.floor((diff -= hours * 3600000) / 60000)
+                let seconds = Math.floor((diff -= minutes * 60000) / 1000)
+                return `${days>0?days+' days, ':''}${hours>0?hours+' hours, ':''}${minutes>0?minutes+' minutes, ':''}${seconds+' seconds'}`
+            }
+        }
+    })()
